5738a6277003d82ed09c57e608c2856038f03fa1
Bachtiar Dwi Pramudi <bchtrrprmd@gmail.com>
2025-11-16 23:46:15 +0700

    create : auth actions handler using Better Auth

---
+++ clients/lib/actions/a.auth.ts
@@ -0,0 +1,34 @@
+"use server";
+
+import { auth } from "../auth";
+import { headers } from "next/headers";
+
+export const signUp = async (email: string, password: string, name: string) => {
+  const result = await auth.api.signUpEmail({
+    body: {
+      email,
+      password,
+      name,
+      callbackURL: "/dashboard",
+    },
+  });
+
+  return result;
+};
+
+export const signIn = async (email: string, password: string) => {
+  const result = await auth.api.signInEmail({
+    body: {
+      email,
+      password,
+      callbackURL: "/dashboard",
+    },
+  });
+
+  return result;
+};
+
+export const signOut = async () => {
+  const result = await auth.api.signOut({ headers: await headers() });
+  return result;
+};
