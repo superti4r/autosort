af8df7e469ea4325f47c1deb72f7cc7e2621746b
Bachtiar Dwi Pramudi <bchtrrprmd@gmail.com>
2025-11-19 10:22:51 +0700

    adjust : to be default

--- clients/app/layout.tsx
+++ clients/app/layout.tsx
@@ -20,7 +20,9 @@ export default function RootLayout({
 }) {
   return (
     <html lang="en">
-      <body className={`${poppins.variable} antialiased`}>{children}</body>
+      <body className={`${poppins.variable} antialiased`}>
+        {children}
+        </body>
     </html>
   );
 }
