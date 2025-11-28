9238fe383be097387ef1f7da9a19d4ef22967d7c
Bachtiar Dwi Pramudi <bchtrrprmd@gmail.com>
2025-11-29 01:55:22 +0700

    update : cors v2

--- app/next.config.ts
+++ app/next.config.ts
@@ -2,9 +2,12 @@ import type { NextConfig } from "next";
 
 const nextConfig: NextConfig = {
   allowedDevOrigins: [
+    'localhost',
     'localhost:3000',
-    '192.168.1.29:3000',
     '192.168.1.29',
+    '192.168.1.29:3000',
+    '0.0.0.0',
+    '0.0.0.0:3000',
   ]
 };
 
