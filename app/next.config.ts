0993e36e8e5e2026702a9bdcdcc23788a45bafc6
Bachtiar Dwi Pramudi <bchtrrprmd@gmail.com>
2025-11-29 01:39:48 +0700

    adjust : cors on nextjs

--- app/next.config.ts
+++ app/next.config.ts
@@ -1,7 +1,11 @@
 import type { NextConfig } from "next";
 
 const nextConfig: NextConfig = {
-  /* config options here */
+  allowedDevOrigins: [
+    "http://localhost:3000",
+    "http://192.168.*.*/",
+    "0.*.*.*",
+  ]
 };
 
 export default nextConfig;
